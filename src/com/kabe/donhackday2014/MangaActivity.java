c8306e145506874f66915b4fccb1ce2a28d1ff19
ariarishosho <rsb10939@nifty.com>
2014-08-09 14:31:46 +0900

    顔当てはめる機能追加

--- src/com/kabe/donhackday2014/MangaActivity.java
+++ src/com/kabe/donhackday2014/MangaActivity.java
@@ -3,13 +3,20 @@ package com.kabe.donhackday2014;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
 
-public class MangaActivity extends Activity{
+import com.kabe.donhackday2014.listener.DragViewListener;
+
+public class MangaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_manga);
+		// ドラッグ対象Viewとイベント処理クラスを紐付ける
+		ImageView dragView = (ImageView) findViewById(R.id.imageView1);
+		DragViewListener listener = new DragViewListener(dragView);
+		dragView.setOnTouchListener(listener);
 	}
 
 }
