08106eb96d57852435ad94a7d7b69ebd8c1e2b18
河 宜成 <takoucompany@gmail.com>
2014-08-10 17:13:12 +0900

    fixed api level

--- src/com/kabe/donhackday2014/MainActivity.java
+++ src/com/kabe/donhackday2014/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		findViewById(R.id.linearManga).setOnClickListener(this);
 
 		// DEBUG用 本番のときは消しましょう！
-		findViewById(R.id.linearTodayKabedon).setVisibility(View.VISIBLE);
+//		findViewById(R.id.linearTodayKabedon).setVisibility(View.VISIBLE);
 
 	}
 
